0e578b0ce5e372076c3ec0b50fcfa9a5ce6c7745
Juan Camilo <camilohnz78@gmail.com>
2026-05-31 18:24:08 -0500

    Config: Add Render domain to ALLOWED_HOSTS

--- costosproyectos/settings.py
+++ costosproyectos/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
